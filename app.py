baad3ae31e6a991005436c78b90bd4c2b3e32b6c
Ole Voss <jan-ole.voss@stud.hs-emden-leer.de>
2022-07-10 14:13:51 +0200

    Git integration test

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", x="Hallo Mirco!")
+
 
 if __name__ == '__main__':
     app.run(port=1337, debug=True)
